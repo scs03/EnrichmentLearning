47f163aaee605aa46bb1fa986fd4cd453ccfbd71
Sriram Sendhil <sriramsendhil@gmail.com>
2024-06-25 23:02:41 -0500

    Sriram: updated login page designs

--- app/login.tsx
+++ app/login.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from 'react'
 import Link from 'next/link';
+import React, { useState } from 'react';
 
 const Login = () => {
+  
   return (
     // <div className="min-h-screen flex justify-center items-center bg-base-100">
     //   <div className="card lg:card-side text-Sky-100 bg-accent w-1/3 h-auto shadow-2xl  rounded-lg">
@@ -28,30 +29,33 @@ const Login = () => {
     //     </div>
     //   </div>
     // </div>
-    <div className="flex flex-col min-h-screen">
+
+    //
+
+    
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-primary  to-base-100">
       <header className="h-24 bg-primary bg-repeat">
         {/* Top design as background */}
+        <h1 className='pl-5 font-bold flex text-4xl items-center min-h-full'>FISD. Enrichment Learning</h1>
       </header>
       <main className="flex-grow flex items-center justify-center bg-bottom bg-repeat-x" style={{ backgroundImage: "url('/bottom-pattern.svg')" }}>
-        <div className=" w-96 h-96 text-center -mt-32 border p-7 rounded-xl border-1.75 hover:border-secondary ">
-          <h1 className="text-4xl font-bold">Sign In</h1>
+        <div className=" w-96 h-96 text-center -mt-32 p-7 rounded-xl border-2 border-base-100 bg-base-100 drop-shadow-2xl">
+          <h1 className="text-4xl  font-bold">SIGN IN</h1>
           {/* Sign in form */}
           <form className="mt-5">
             <input
               type="text"
               placeholder="Username"
-              className="border p-2 rounded w-full max-w-md"
+              className=" border-primary p-2 rounded w-full max-w-md border-2 "
             />
           </form>
           <form className='mt-5'>
             <input type="password"
             placeholder='Password'
-            className='border p-2 rounded w-full max-w-md' 
+            className=' border-primary p-2 rounded w-full max-w-md border-2' 
             />
           </form>
-            <button className="mt-4 min-w-20 p-2 bg-primary text-white rounded">
-              Next
-            </button>
+            <Link className="btn border-2  mt-8 w-full p-2 bg-primary border-primary text-white rounded  hover:text-black hover:bg-transparent" href='/teachers'>Login</Link>
         </div>
       </main>
     </div>
